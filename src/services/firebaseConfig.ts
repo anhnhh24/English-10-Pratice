abe317cad9628cea7076615b0ed44148f0f371e2
anhnhh24 <nhhagp2@gmail.com>
2026-08-24 17:07:20 +0700

    fix env setting

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -4,7 +4,7 @@ import { getDatabase, ref, set, get, onValue, push, remove, update, off, onChild
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCQ6RQv4bH038jJBUWUF-rkwR4Z-P3iXoQ",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "hoanghaassitant.firebaseapp.com",
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://hoanghaassitant-default-rtdb.firebaseio.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://hoanghaassitant-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "hoanghaassitant",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "hoanghaassitant.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "44671692125",
